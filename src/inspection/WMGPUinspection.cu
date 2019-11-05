8ef00ba0bff4f8e78c7d85809ccbc2b5b9cc88b6
Daniel <danielfdeus@gmail.com>
2019-11-05 11:23:22 -0300

    encontra erro

--- src/inspection/WMGPUinspection.cu
+++ src/inspection/WMGPUinspection.cu
@@ -608,10 +608,15 @@ void WMGPUInspection::PrecarregaTabelaHash(std::string strTemp, int hash,char* d
 	
 
 	for(int j=0;j<padroes.size();j++) {
+
+		if(indexDebug == 136600){
+			std::cout <<"for error index j:"<<j<<std::endl;
+		}
+		
         bool hashEsgotadoPadraoAtual = false;
 		std::string strTemp2 = padroes[j];
 
-	
+		
 
 		int idxInicio2 = 0;
 		int idxFim2 = B - 1;
