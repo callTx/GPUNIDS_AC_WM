c4a4714975d886a576cf0b5a2b4b756b6de83154
Daniel <danielfdeus@gmail.com>
2019-11-09 10:20:40 -0300

    encontra erro

--- src/inspection/WMGPUinspection.cu
+++ src/inspection/WMGPUinspection.cu
@@ -800,7 +800,9 @@ void WMGPUInspection::PrecarregaTabelaHash(std::string strTemp, int hash,char* d
                 idxFim2 = idxFim2 + 1;
 			}
 			
-
+			if(indexDebug == 762443){
+				std::cout <<"idxInicio2==idxFim2 end error "<<std::endl;
+			}
 			
 
 		}
